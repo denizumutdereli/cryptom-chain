046997a6c242e03fe5bae52974e2b6c865493cc7
Deniz Umut Dereli <197686+denizumutdereli@users.noreply.github.com>
2022-05-20 00:31:02 +0300

    difficulty volatlty guard fixed to 2 zeros.

--- blockchain.js
+++ blockchain.js
@@ -25,11 +25,15 @@ class Blockchain {
 
             const actualLastHash = chain[i - 1].hash;
 
+            const lastDifficulty = chain[i - 1].difficulty
+
             if (lastHash !== actualLastHash) { return false; }
 
             const validatedHash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
 
             if (hash !== validatedHash) { return false; }
+
+            if((Math.abs(lastDifficulty - difficulty)) > 2) return false; //max difficulty change fixed to 2 zeros
         }
 
         return true;
--- blockchain.test.js
+++ blockchain.test.js
@@ -1,5 +1,6 @@
 const Blockchain = require('./blockchain');
 const Block = require('./block');
+const CryptoHash = require('./crypto-hash');
 
 describe('Blockchain', () => {
     let blockchain, nodeChain, originalChain;
@@ -62,6 +63,25 @@ describe('Blockchain', () => {
                 it('returns true', () => {
                     expect(Blockchain.isValidChain(blockchain.chain)).toBe(true);
 
+                });
+            });
+
+            describe('difficulty volatility jumps', () => {
+                it('return false', () => {
+                    const lastBlock = blockchain.chain[blockchain.chain.length - 1];
+                    const lastHash = lastBlock.hash;
+                    const timestamp = Date.now();
+                    const nonce = 0;
+                    const data = [];
+                    const difficulty = lastBlock.difficulty - 3;
+
+                    const hash = CryptoHash(timestamp, lastHash, difficulty, nonce, data);
+
+                    const badBlock = new Block({timestamp, lastHash, hash, nonce, difficulty, data});
+
+                    blockchain.chain.push(badBlock);
+
+                    expect(Blockchain.isValidChain(blockchain.chain)).toBe(false);
                 });
             });
         });
