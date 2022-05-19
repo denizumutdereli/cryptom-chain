e37d01bc73f3bb40cabbfec3c74f55ae85c8e1e9
Deniz Umut Dereli <197686+denizumutdereli@users.noreply.github.com>
2022-05-19 22:36:59 +0300

    nonce fixed

--- block.test.js
+++ block.test.js
@@ -7,11 +7,15 @@ describe('Block', () => {
     const lastHash = 'foo-hash';
     const hash = 'bar-hash';
     const data = ['blockchain', 'data'];
+    const nonce = 1;
+    const difficulty = 1;
     const block = new Block({
         timestamp,
         lastHash,
         hash,
-        data
+        data,
+        nonce,
+        difficulty
     });
 
     it('has a timestamp, lastHash, hash and data propoerty', () => {
@@ -19,6 +23,8 @@ describe('Block', () => {
         expect(block.lastHash).toEqual(lastHash);
         expect(block.hash).toEqual(hash);
         expect(block.data).toEqual(data);
+        expect(block.nonce).toEqual(nonce);
+        expect(block.difficulty).toEqual(difficulty);
     });
 
     //genesis block
@@ -26,7 +32,7 @@ describe('Block', () => {
         const genesisBlock = Block.genesis();
 
         //console.log('genesisBlock:', genesisBlock);
-        
+
         it('returns a Block instance', () => {
             expect(genesisBlock instanceof Block).toBe(true);
         });
@@ -61,7 +67,20 @@ describe('Block', () => {
         });
 
         it('creates a SHA256 `hash` based on the proper inputs', () => {
-            expect(minedBlock.hash).toEqual(cryptoHash(minedBlock.timestamp, lastBlock.hash, data));
+            expect(minedBlock.hash).toEqual(
+                cryptoHash(
+                    minedBlock.timestamp,
+                    lastBlock.hash,
+                    minedBlock.nonce,
+                    minedBlock.difficulty,
+                    data
+                )
+            );
+        });
+
+        it('test if `hash` that matches the difficulty of the block', () => {
+            expect(minedBlock.hash.substring(0, minedBlock.difficulty))
+                .toEqual('0'.repeat(minedBlock.difficulty));
         });
     });
     //..ends
