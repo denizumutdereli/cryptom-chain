767636b726145d9618acb653893f102351b52dc2
Deniz Umut Dereli <197686+denizumutdereli@users.noreply.github.com>
2022-05-20 17:24:54 +0300

    api listening port debug..

--- api/config/db.config.js
+++ api/config/db.config.js
@@ -12,7 +12,7 @@ const mongodb_crendentials = process.env.mongodb_crendentials
 module.exports = () => {
     mongoose.connect('mongodb://'+mongodb_crendentials+'@localhost:27017/apidb?authMechanism=DEFAULT&authSource=admin', mongoOptions)
     mongoose.connection.on('open', ()=> {
-        console.log('connected!')
+        //console.log('connected!')
     })
     mongoose.connection.on('error', (err)=> {
         console.log('MongoDB: Not Connected ' + err);
